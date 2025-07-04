ba8dc764f628b0e8bd5da386330022346b3be2c4
ahsam <ah.samba12@gmail.com>
2025-07-04 05:51:32 +0300

    update name app

--- lib/settings.dart
+++ lib/settings.dart
@@ -27,7 +27,7 @@ class PaymentGateway {
 
 class AppSettings {
   /// Basic Settings
-  static const String applicationName = ' بريق ';
+  static const String applicationName = ' بريق2 ';
   static const String packageName = 'sd.bareeq.ads';
   static const String shareAppText = "Share this App";
 
